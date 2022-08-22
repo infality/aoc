261d64198d606498a8564207e631681ea7fa3765
Infality <infality@420blaze.it>
2022-08-22 16:56:20 +0200

    update

--- aoc_2019/src/day08.rs
+++ aoc_2019/src/day08.rs
@@ -1,9 +1,75 @@
 pub fn task1(input: &str) {
     let mut result = 0usize;
+
+    let width = 25;
+    let height = 6;
+    let mut layers = Vec::new();
+    let mut current_layer = [[0; 25]; 6];
+    let chars: Vec<char> = input.chars().collect();
+    let mut i = 0;
+    while i < chars.len() {
+        for y in 0..height {
+            for x in 0..width {
+                current_layer[y][x] = chars[i].to_digit(10).unwrap();
+                i += 1;
+            }
+        }
+        layers.push(current_layer);
+        current_layer = [[0; 25]; 6];
+    }
+
+    let mut min_zeros = usize::MAX;
+    for layer in layers.iter() {
+        let mut zeros = 0;
+        let mut ones = 0;
+        let mut twos = 0;
+        for y in 0..height {
+            for x in 0..width {
+                if layer[y][x] == 0 {
+                    zeros += 1;
+                } else if layer[y][x] == 1 {
+                    ones += 1;
+                } else if layer[y][x] == 2 {
+                    twos += 1;
+                }
+            }
+        }
+        if zeros < min_zeros {
+            min_zeros = zeros;
+            result = ones * twos;
+        }
+    }
+
     println!("Result: {}", result);
 }
 
 pub fn task2(input: &str) {
-    let mut result = 0usize;
-    println!("Result: {}", result);
+    let width = 25;
+    let height = 6;
+    let mut layers = Vec::new();
+    let mut current_layer = [[0; 25]; 6];
+    let chars: Vec<char> = input.chars().collect();
+    let mut i = 0;
+    while i < chars.len() {
+        for y in 0..height {
+            for x in 0..width {
+                current_layer[y][x] = chars[i].to_digit(10).unwrap();
+                i += 1;
+            }
+        }
+        layers.push(current_layer);
+        current_layer = [[0; 25]; 6];
+    }
+
+    for y in 0..height {
+        for x in 0..width {
+            for layer in layers.iter() {
+                if layer[y][x] != 2 {
+                    print!("{}", if layer[y][x] == 1 { 'X' } else { ' ' });
+                    break;
+                }
+            }
+        }
+        println!();
+    }
 }
--- aoc_2019/src/day09.rs
+++ aoc_2019/src/day09.rs
@@ -1,9 +1,134 @@
-pub fn task1(input: &str) {
-    let mut result = 0usize;
-    println!("Result: {}", result);
+use std::collections::HashMap;
+
+fn get(map: &HashMap<usize, isize>, i: usize) -> isize {
+    *map.get(&i).unwrap_or(&0)
 }
 
-pub fn task2(input: &str) {
-    let mut result = 0usize;
-    println!("Result: {}", result);
+pub fn task1(input: &str) {
+    let mut fields: HashMap<usize, isize> = HashMap::new();
+    for (i, s) in input.split(',').enumerate() {
+        fields.insert(i, s.parse().unwrap());
+    }
+
+    let mut counter = 0;
+    let mut relative_base = 0isize;
+    loop {
+        let op_code = get(&fields, counter);
+        if op_code == 99 {
+            break;
+        }
+
+        let op1 = match digit_at_pos(op_code as usize, 2) {
+            0 => get(&fields, get(&fields, counter + 1) as usize),
+            1 => get(&fields, counter + 1),
+            2 => get(
+                &fields,
+                (relative_base + get(&fields, counter + 1)) as usize,
+            ),
+            _ => panic!(),
+        };
+
+        let mut op2 = 0;
+        if op_code % 100 != 3 && op_code % 100 != 4 && op_code % 100 != 9 {
+            op2 = match digit_at_pos(op_code as usize, 3) {
+                0 => get(&fields, get(&fields, counter + 2) as usize),
+                1 => get(&fields, counter + 2),
+                2 => get(
+                    &fields,
+                    (relative_base + get(&fields, counter + 2)) as usize,
+                ),
+                _ => panic!(),
+            };
+        }
+
+        match op_code % 100 {
+            1 => {
+                let dest = get(&fields, counter + 3)
+                    + if digit_at_pos(op_code as usize, 4) == 2 {
+                        relative_base
+                    } else {
+                        0
+                    };
+
+                fields.insert(dest as usize, op1 + op2);
+                counter += 4;
+            }
+            2 => {
+                let dest = get(&fields, counter + 3)
+                    + if digit_at_pos(op_code as usize, 4) == 2 {
+                        relative_base
+                    } else {
+                        0
+                    };
+
+                fields.insert(dest as usize, op1 * op2);
+                counter += 4;
+            }
+            3 => {
+                println!("Input:");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).unwrap();
+
+                let dest = get(&fields, counter + 1)
+                    + if digit_at_pos(op_code as usize, 2) == 2 {
+                        relative_base
+                    } else {
+                        0
+                    };
+                fields.insert(dest as usize, input.trim().parse().unwrap());
+                counter += 2;
+            }
+            4 => {
+                println!("{}", op1);
+                counter += 2;
+            }
+            5 => {
+                if op1 != 0 {
+                    counter = op2 as usize;
+                } else {
+                    counter += 3;
+                }
+            }
+            6 => {
+                if op1 == 0 {
+                    counter = op2 as usize;
+                } else {
+                    counter += 3;
+                }
+            }
+            7 => {
+                let dest = get(&fields, counter + 3)
+                    + if digit_at_pos(op_code as usize, 4) == 2 {
+                        relative_base
+                    } else {
+                        0
+                    };
+
+                fields.insert(dest as usize, if op1 < op2 { 1 } else { 0 });
+                counter += 4;
+            }
+            8 => {
+                let dest = get(&fields, counter + 3)
+                    + if digit_at_pos(op_code as usize, 4) == 2 {
+                        relative_base
+                    } else {
+                        0
+                    };
+
+                fields.insert(dest as usize, if op1 == op2 { 1 } else { 0 });
+                counter += 4;
+            }
+            9 => {
+                relative_base += op1;
+                counter += 2;
+            }
+            _ => panic!("{}", op_code),
+        }
+    }
+}
+
+pub fn task2(_input: &str) {}
+
+fn digit_at_pos(num: usize, pos: usize) -> usize {
+    (num / 10_usize.pow(pos as u32)) % 10
 }
