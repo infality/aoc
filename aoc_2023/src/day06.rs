b8592e4a94ab19dfd8fe1678774e43e5ddb8168e
Infality <infality@420blaze.it>
2023-12-06 20:02:31 +0100

    update

--- aoc_2023/src/day06.rs
+++ aoc_2023/src/day06.rs
@@ -1,9 +1,60 @@
+fn parse_line(line: &str) -> Vec<usize> {
+    line.split_once(":")
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect()
+}
+
 pub fn task1(input: &str) {
-    let mut result = 0usize;
+    let mut result = 1usize;
+
+    let times = parse_line(input.lines().nth(0).unwrap());
+    let distances = parse_line(input.lines().nth(1).unwrap());
+
+    for (time, distance) in times.iter().zip(distances) {
+        let mut ways = 0;
+        for holding in 1..*time {
+            if (time - holding) * holding > distance {
+                ways += 1;
+            }
+        }
+        result *= ways;
+    }
+
     println!("Result: {}", result);
 }
 
 pub fn task2(input: &str) {
     let mut result = 0usize;
+
+    let time: usize = input
+        .lines()
+        .nth(0)
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .replace(" ", "")
+        .parse()
+        .unwrap();
+    let distance: usize = input
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .replace(" ", "")
+        .parse()
+        .unwrap();
+
+    for holding in 1..time {
+        if (time - holding) * holding > distance {
+            result += 1;
+        }
+    }
+
     println!("Result: {}", result);
 }
