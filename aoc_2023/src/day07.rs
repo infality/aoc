427030d12672ade2caf645c6ca2969db5b4ef95a
Infality <infality@420blaze.it>
2023-12-08 16:41:02 +0100

    update

--- aoc_2023/src/day07.rs
+++ aoc_2023/src/day07.rs
@@ -1,9 +1,159 @@
+use std::collections::HashMap;
+
+fn get_type(hand: &str) -> usize {
+    assert!(hand.len() == 5);
+    let mut card_counts = HashMap::new();
+    for card in hand.chars() {
+        card_counts.entry(card).and_modify(|x| *x += 1).or_insert(1);
+    }
+
+    if card_counts.values().any(|x| *x == 5) {
+        return 6;
+    }
+
+    if card_counts.values().any(|x| *x == 4) {
+        return 5;
+    }
+
+    if card_counts.values().any(|x| *x == 3) && card_counts.values().any(|x| *x == 2) {
+        return 4;
+    }
+
+    if card_counts.values().any(|x| *x == 3) {
+        return 3;
+    }
+
+    if card_counts.values().filter(|x| **x == 2).count() == 2 {
+        return 2;
+    }
+
+    if card_counts.values().any(|x| *x == 2) {
+        return 1;
+    }
+    0
+}
+
+fn get_rank(card: char) -> usize {
+    if card.is_digit(10) {
+        return card.to_digit(10).unwrap() as usize;
+    }
+    match card {
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => unreachable!(),
+    }
+}
+
 pub fn task1(input: &str) {
     let mut result = 0usize;
+
+    let mut hands: Vec<(&str, usize)> = input
+        .lines()
+        .map(|x| x.trim().split_once(" ").unwrap())
+        .map(|x| (x.0, x.1.parse().unwrap()))
+        .collect();
+
+    hands.sort_unstable_by(|a, b| {
+        if get_type(a.0) != get_type(b.0) {
+            return get_type(a.0).cmp(&get_type(b.0));
+        }
+        for (ac, bc) in a.0.chars().zip(b.0.chars()) {
+            if ac == bc {
+                continue;
+            }
+            return get_rank(ac).cmp(&get_rank(bc));
+        }
+        std::cmp::Ordering::Equal
+    });
+
+    for (i, hand) in hands.iter().enumerate() {
+        result += (i + 1) * hand.1;
+    }
+
     println!("Result: {}", result);
+}
+
+fn get_type2(hand: &str) -> usize {
+    assert!(hand.len() == 5);
+    let mut card_counts = HashMap::new();
+    for card in hand.chars() {
+        card_counts.entry(card).and_modify(|x| *x += 1).or_insert(1);
+    }
+
+    if card_counts.keys().any(|x| *x != 'J') {
+        if let Some(j_count) = card_counts.remove(&'J') {
+            *card_counts.iter_mut().max_by_key(|x| *x.1).unwrap().1 += j_count;
+        }
+    }
+
+    if card_counts.values().any(|x| *x == 5) {
+        return 6;
+    }
+
+    if card_counts.values().any(|x| *x == 4) {
+        return 5;
+    }
+
+    if card_counts.values().any(|x| *x == 3) && card_counts.values().any(|x| *x == 2) {
+        return 4;
+    }
+
+    if card_counts.values().any(|x| *x == 3) {
+        return 3;
+    }
+
+    if card_counts.values().filter(|x| **x == 2).count() == 2 {
+        return 2;
+    }
+
+    if card_counts.values().any(|x| *x == 2) {
+        return 1;
+    }
+    0
+}
+
+fn get_rank2(card: char) -> usize {
+    if card.is_digit(10) {
+        return card.to_digit(10).unwrap() as usize;
+    }
+    match card {
+        'T' => 10,
+        'J' => 0,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => unreachable!(),
+    }
 }
 
 pub fn task2(input: &str) {
     let mut result = 0usize;
+
+    let mut hands: Vec<(&str, usize)> = input
+        .lines()
+        .map(|x| x.trim().split_once(" ").unwrap())
+        .map(|x| (x.0, x.1.parse().unwrap()))
+        .collect();
+
+    hands.sort_unstable_by(|a, b| {
+        if get_type2(a.0) != get_type2(b.0) {
+            return get_type2(a.0).cmp(&get_type2(b.0));
+        }
+        for (ac, bc) in a.0.chars().zip(b.0.chars()) {
+            if ac == bc {
+                continue;
+            }
+            return get_rank2(ac).cmp(&get_rank2(bc));
+        }
+        std::cmp::Ordering::Equal
+    });
+
+    for (i, hand) in hands.iter().enumerate() {
+        result += (i + 1) * hand.1;
+    }
+
     println!("Result: {}", result);
 }
--- aoc_2023/src/day08.rs
+++ aoc_2023/src/day08.rs
@@ -1,9 +1,124 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn task1(input: &str) {
     let mut result = 0usize;
+
+    let parts = input.split_once("\n\n").unwrap();
+    let instructions = parts.0;
+
+    let mut map = HashMap::new();
+    for (i, line) in parts.1.lines().enumerate() {
+        let name = line.split_once(" = ").unwrap().0;
+        map.insert(name, i);
+    }
+    let start = *map.get("AAA").unwrap();
+    let end = *map.get("ZZZ").unwrap();
+
+    let mut nodes = Vec::new();
+    for line in parts.1.lines() {
+        let ways = line.split_once(" = ").unwrap().1.split_once(", ").unwrap();
+        let left = map.get(&ways.0[1..]).unwrap();
+        let right = map.get(&ways.1[..3]).unwrap();
+        nodes.push([*left, *right]);
+    }
+
+    let mut current = start;
+    loop {
+        let direction = match instructions
+            .chars()
+            .nth(result % instructions.len())
+            .unwrap()
+        {
+            'L' => 0,
+            'R' => 1,
+            _ => unreachable!(),
+        };
+        result += 1;
+        current = nodes[current][direction];
+        if current == end {
+            break;
+        }
+    }
+
     println!("Result: {}", result);
 }
 
 pub fn task2(input: &str) {
     let mut result = 0usize;
+
+    let parts = input.split_once("\n\n").unwrap();
+    let instructions = parts.0;
+
+    let mut map = HashMap::new();
+    let mut current_nodes = Vec::new();
+    let mut ends = HashSet::new();
+    for (i, line) in parts.1.lines().enumerate() {
+        let name = line.split_once(" = ").unwrap().0;
+        map.insert(name, i);
+        if name.contains('A') {
+            current_nodes.push(i);
+        }
+        if name.contains('Z') {
+            ends.insert(i);
+        }
+    }
+
+    let mut nodes = Vec::new();
+    for line in parts.1.lines() {
+        let ways = line.split_once(" = ").unwrap().1.split_once(", ").unwrap();
+        let left = map.get(&ways.0[1..]).unwrap();
+        let right = map.get(&ways.1[..3]).unwrap();
+        nodes.push([*left, *right]);
+    }
+
+    let mut cycles = vec![None; current_nodes.len()];
+    loop {
+        let direction = match instructions
+            .chars()
+            .nth(result % instructions.len())
+            .unwrap()
+        {
+            'L' => 0,
+            'R' => 1,
+            _ => unreachable!(),
+        };
+        result += 1;
+        for (i, current) in current_nodes.iter_mut().enumerate() {
+            *current = nodes[*current][direction];
+            if cycles[i].is_none() && ends.contains(current) {
+                cycles[i] = Some(result);
+            }
+        }
+        if cycles.iter().all(|x| x.is_some()) {
+            break;
+        }
+    }
+
+    result = cycles[0].unwrap();
+    for cycle in cycles.iter().skip(1) {
+        result = lcm(result, cycle.unwrap());
+    }
+
     println!("Result: {}", result);
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    if a == b {
+        return a;
+    }
+    if b > a {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    while b > 0 {
+        let temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    return a * (b / gcd(a, b));
 }
