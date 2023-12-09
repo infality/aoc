273239e0ec0443bdcd2a262052c7919556460400
Infality <infality@420blaze.it>
2023-12-09 12:39:12 +0100

    update

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 - **2018:** 01-25
 - **2019:** 11-25
 - **2022:** 02-25
+
+Some days from 2020 and 2021 are not here since I did them in python and zig.
--- aoc_2023/src/day09.rs
+++ aoc_2023/src/day09.rs
@@ -1,9 +1,60 @@
 pub fn task1(input: &str) {
-    let mut result = 0usize;
+    let mut result = 0isize;
+
+    let histories: Vec<Vec<isize>> = input
+        .lines()
+        .map(|x| x.split_whitespace().map(|y| y.parse().unwrap()).collect())
+        .collect();
+
+    for history in histories {
+        let mut seqs = Vec::new();
+        seqs.push(history);
+        while seqs.last().unwrap().iter().any(|y| *y != 0) {
+            let mut seq = Vec::new();
+            for window in seqs.last().unwrap().windows(2) {
+                seq.push(window[1] - window[0]);
+            }
+            seqs.push(seq);
+        }
+
+        seqs.last_mut().unwrap().push(0);
+        for i in (0..seqs.len() - 1).rev() {
+            let j = seqs[i].len() - 1;
+            let value = seqs[i][j] + seqs[i + 1][j];
+            seqs[i].push(value);
+        }
+        result += *seqs.first().unwrap().last().unwrap();
+    }
+
     println!("Result: {}", result);
 }
 
 pub fn task2(input: &str) {
-    let mut result = 0usize;
+    let mut result = 0isize;
+
+    let histories: Vec<Vec<isize>> = input
+        .lines()
+        .map(|x| x.split_whitespace().map(|y| y.parse().unwrap()).collect())
+        .collect();
+
+    for history in histories {
+        let mut seqs = Vec::new();
+        seqs.push(history);
+        while seqs.last().unwrap().iter().any(|y| *y != 0) {
+            let mut seq = Vec::new();
+            for window in seqs.last().unwrap().windows(2) {
+                seq.push(window[1] - window[0]);
+            }
+            seqs.push(seq);
+        }
+
+        seqs.last_mut().unwrap().push(0);
+        for i in (0..seqs.len() - 1).rev() {
+            let value = seqs[i][0] - seqs[i + 1].last().unwrap();
+            seqs[i].push(value);
+        }
+        result += *seqs.first().unwrap().last().unwrap();
+    }
+
     println!("Result: {}", result);
 }
